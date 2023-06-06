f6c24db1b0e9e0a1a20f72824105576fadbf9311
Angel Leon <angel@leanware.io>
2023-06-05 23:23:12 -0500

    update production url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://portfolio-backend-angel.up.railway.app',
+  apiUrl: 'https://waterbnb-backend-production.up.railway.app',
   CLOUDINARY_URL: 'https://api.cloudinary.com/v1_1/dev-empty/image/upload',
 };
